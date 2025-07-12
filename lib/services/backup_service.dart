6b312f0f284790de7dd658eae1e5dda1d3a44140
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-07-12 16:31:06 -0300

    feat: update share parameters in BackupService to remove text field for database backup

--- lib/services/backup_service.dart
+++ lib/services/backup_service.dart
@@ -31,7 +31,7 @@ class BackupService {
         ShareParams(
           files: [XFile(exportPath)],
           fileNameOverrides: ['assets_backup.db'],
-          text: 'Backup do banco de dados (.db)',
+          // text: 'Backup do banco de dados (.db)',
           sharePositionOrigin: Rect.zero,
         ),
       );
