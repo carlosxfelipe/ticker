75a82e6446bab0e781034132752e95d358f4e1b7
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-07-13 09:30:39 -0300

    feat: add copyright and licensing information to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,19 @@
+// Ticker - Financial asset management application
+// Copyright (C) 2025 Carlos Felipe Araújo
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:ticker/theme/theme.dart';
