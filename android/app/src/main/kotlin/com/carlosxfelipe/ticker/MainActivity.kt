2bf37d39727bce1da3f430a1f93c2235fc939131
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-07-14 22:55:00 -0300

    feat: change MainActivity to extend FlutterFragmentActivity

--- android/app/src/main/kotlin/com/carlosxfelipe/ticker/MainActivity.kt
+++ android/app/src/main/kotlin/com/carlosxfelipe/ticker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.carlosxfelipe.ticker
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
+
